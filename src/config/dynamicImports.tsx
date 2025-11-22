44422a3824b458080fd3381ca15e3336df0228ee
Lypezin <Foolype@gmail.com>
2025-11-22 00:27:54 -0300

    K

--- src/config/dynamicImports.tsx
+++ src/config/dynamicImports.tsx
@@ -5,63 +5,86 @@
 import React from 'react';
 import dynamic from 'next/dynamic';
 
-const loadingComponent = (
+const defaultLoading = (
   <div className="flex h-64 items-center justify-center">
     <div className="h-8 w-8 animate-spin rounded-full border-4 border-blue-200 border-t-blue-600"></div>
   </div>
 );
 
-const dynamicOptions = {
-  ssr: false,
-  loading: () => loadingComponent,
-};
+const marketingLoading = (
+  <div className="flex h-64 items-center justify-center">
+    <div className="h-8 w-8 animate-spin rounded-full border-4 border-purple-200 border-t-purple-600"></div>
+  </div>
+);
 
 export const DashboardView = dynamic(
   () => import('@/components/views/DashboardView').then(mod => ({ default: mod.default })),
-  dynamicOptions
+  {
+    ssr: false,
+    loading: () => defaultLoading,
+  }
 );
 
 export const MarketingView = dynamic(
   () => import('@/components/views/MarketingView').then(mod => ({ default: mod.default })),
-  { ...dynamicOptions, loading: () => (
-    <div className="flex h-64 items-center justify-center">
-      <div className="h-8 w-8 animate-spin rounded-full border-4 border-purple-200 border-t-purple-600"></div>
-    </div>
-  )}
+  {
+    ssr: false,
+    loading: () => marketingLoading,
+  }
 );
 
 export const AnaliseView = dynamic(
   () => import('@/components/views/AnaliseView').then(mod => ({ default: mod.default })),
-  dynamicOptions
+  {
+    ssr: false,
+    loading: () => defaultLoading,
+  }
 );
 
 export const UtrView = dynamic(
   () => import('@/components/views/UtrView').then(mod => ({ default: mod.default })),
-  dynamicOptions
+  {
+    ssr: false,
+    loading: () => defaultLoading,
+  }
 );
 
 export const EvolucaoView = dynamic(
   () => import('@/components/views/EvolucaoView').then(mod => ({ default: mod.default })),
-  dynamicOptions
+  {
+    ssr: false,
+    loading: () => defaultLoading,
+  }
 );
 
 export const ValoresView = dynamic(
   () => import('@/components/views/ValoresView').then(mod => ({ default: mod.default })),
-  dynamicOptions
+  {
+    ssr: false,
+    loading: () => defaultLoading,
+  }
 );
 
 export const EntregadoresMainView = dynamic(
   () => import('@/components/views/EntregadoresMainView').then(mod => ({ default: mod.default })),
-  dynamicOptions
+  {
+    ssr: false,
+    loading: () => defaultLoading,
+  }
 );
 
 export const PrioridadePromoView = dynamic(
   () => import('@/components/views/PrioridadePromoView').then(mod => ({ default: mod.default })),
-  dynamicOptions
+  {
+    ssr: false,
+    loading: () => defaultLoading,
+  }
 );
 
 export const ComparacaoView = dynamic(
   () => import('@/components/views/ComparacaoView').then(mod => ({ default: mod.default })),
-  dynamicOptions
+  {
+    ssr: false,
+    loading: () => defaultLoading,
+  }
 );
-
