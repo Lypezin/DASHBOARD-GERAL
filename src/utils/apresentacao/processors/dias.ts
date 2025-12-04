1a0e4e9ed9b24ec42dfc8cd4368d140049e0c22e
lypezin <foolype@gmail.com>
2025-12-04 16:14:12 -0300

    K

--- src/utils/apresentacao/processors/dias.ts
+++ src/utils/apresentacao/processors/dias.ts
@@ -9,15 +9,14 @@ import {
 } from './common';
 import { DadosBasicos } from './basicData';
 
+import { findDayData } from '@/utils/comparacaoHelpers';
+
 export const processarDias = (dadosBasicos: DadosBasicos) => {
     const { semana1, semana2 } = dadosBasicos;
     if (!semana1 || !semana2) return { semana1Dias: [], semana2Dias: [] };
 
-    const diasSemana1Map = new Map((semana1.dia || []).map((item) => [item.dia_da_semana, item]));
-    const diasSemana2Map = new Map((semana2.dia || []).map((item) => [item.dia_da_semana, item]));
-
     const semana1Dias = diasOrdem.map((dia) => {
-        const info = diasSemana1Map.get(dia) || ({} as any);
+        const info = findDayData(dia, semana1.dia) || ({} as any);
         const horas = info?.segundos_realizados
             ? info.segundos_realizados / 3600
             : converterHorasParaDecimal(info?.horas_entregues || '0');
@@ -30,8 +29,8 @@ export const processarDias = (dadosBasicos: DadosBasicos) => {
     });
 
     const semana2Dias = diasOrdem.map((dia) => {
-        const info1 = diasSemana1Map.get(dia) || ({} as any);
-        const info2 = diasSemana2Map.get(dia) || ({} as any);
+        const info1 = findDayData(dia, semana1.dia) || ({} as any);
+        const info2 = findDayData(dia, semana2.dia) || ({} as any);
 
         const horas1 = info1?.segundos_realizados
             ? info1.segundos_realizados / 3600
