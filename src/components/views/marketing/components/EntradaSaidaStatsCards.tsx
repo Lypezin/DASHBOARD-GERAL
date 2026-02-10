5d40e5fd491812671bb4a4da8c6e62082134da91
lypezin <foolype@gmail.com>
2026-02-10 18:22:50 -0300

    k

--- src/components/views/marketing/components/EntradaSaidaStatsCards.tsx
+++ src/components/views/marketing/components/EntradaSaidaStatsCards.tsx
@@ -11,7 +11,7 @@ export const EntradaSaidaStatsCards: React.FC<EntradaSaidaStatsCardsProps> = ({ 
     const { totals, saldo_total, saldo_marketing, saldo_operacional, formatPercent } = useEntradaSaidaTotals(data);
 
     return (
-        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-5">
+        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
             {/* ENTRADAS */}
             <MarketingStatsCard
                 title="Entradas"
